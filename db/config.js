a6bec8e1ac84002e7e8545a038b4d8c438ebc1db
farhin_mansuri <farhinbanu.mansuri@geniobits.in>
2026-09-15 21:29:48 +0530

    Change in config

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
-const dns = require("dns");
+//const dns = require("dns");
 const mongoose = require('mongoose')
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
+//dns.setServers(["8.8.8.8", "1.1.1.1"]); // this is for local
 mongoose.connect(process.env.MONGOOSE_URL)
     .then(() => console.log('Successfully connected to MongoDB.'))
     .catch(err => console.error('MongoDB connection error:', err));

1209c3a2083b7a4cdd21ab1cbd94955d05d7a103
farhin_mansuri <farhinbanu.mansuri@geniobits.in>
2026-09-15 21:50:56 +0530

    Change in config -2

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ const io = new Server(server, {
 socketHandler(io);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0",() => {
     console.log(`Server running on port ${PORT}`);
 });

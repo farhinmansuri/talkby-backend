08ad8109290cdb0ab8a95735dae2ee20dfe41f3f
farhin_mansuri <farhinbanu.mansuri@geniobits.in>
2026-09-19 15:52:38 +0530

    change in nodemailer

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
